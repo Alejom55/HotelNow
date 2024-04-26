1095bade346cf05375e37769e73fd481ae387c36
alejom55 <alejandromarin203@gmail.com>
2024-04-26 15:35:44 -0500

    actualizacion

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,23 @@
 import { useState } from 'react'
 import './App.css'
+import ShowProducts from './components/ShowProducts'
+import CreateProduct from './components/CreateProduct'
+import EditProduct from './components/EditProduct'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <button className="">uwu</button>
-        </header>
-
+      <div className="App font-haken">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<ShowProducts />} />
+            {/* <Route path='/create' element={<CreateProduct />} /> */}
+            {/* <Route path='/edit/:id' element={<EditProduct />} /> */}
+          </Routes>
+        </BrowserRouter>
       </div>
     </>
   )
--- client/src/components/ShowProducts.jsx
+++ client/src/components/ShowProducts.jsx
@@ -2,19 +2,50 @@ import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
-const endpoint = 'http://localhost:3001/api'
-
-function ShowProducts() {
-
+const endpoint = `http://localhost:8000/api`
+// const endpoint = `${process.env.REACT_APP_AXIOS_URL}/api`
+const ShowProducts = () => {
+    // console.log(process.env.REACT_PUBLIC_AXIOS_URL)
     const [products, setProducts] = useState([])
     useEffect(() => {
         getAllProducts()
     }, [])
 
-    const getAllProducts = async () => { }
-    const deleteProduct = async (id) => { }
+    const getAllProducts = async () => {
+        const res = await axios.get(`${endpoint}/products`)
+        setProducts(res.data)
+    }
+
+    const deleteProduct = async (id) => {
+        await axios.delete(`${endpoint}/product/${id}`)
+        getAllProducts()
+    }
     return (
-        <div>Tabla</div>
+        <div>
+            <table className=''>
+                <thead>
+                    <tr>Descripcion</tr>
+                    <tr>Price</tr>
+                    <tr>Stock</tr>
+                    <tr>Actions</tr>
+                </thead>
+                <tbody>
+                    {products.map((product, index) => (
+                        <tr key={product.id}>
+                            <td>{product.description}</td>
+                            <td>{product.price}</td>
+                            <td>{product.price}</td>
+                            <td>{product.stock}</td>
+                            <td>
+                                <Link to={`/edit/${product.id}`} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded'>Edit</Link>
+                                <button onClick={() => deleteProduct(product.id)} className='btn btn-danger'>Delete</button>
+                            </td>
+                        </tr>
+
+                    ))}
+                </tbody>
+            </table>
+        </div>
     )
 }
 
